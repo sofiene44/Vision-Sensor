5c896a4ee4af8330e48cc3879dbaaa051e261927
sofiene44 <sofieneslama@live.fr>
2019-05-27 15:24:46 +0100

    VisionSensor raspberryPi version

--- raspberryPiVisionSensor.py
+++ raspberryPiVisionSensor.py
@@ -1,22 +1,31 @@
-from RPiSim.GPIO import GPIO
+#from RPiSim.GPIO import GPIO
+from RPi import GPIO
+
+
 from modules.ProcessingTools import ProcessingTools
 from modules.CaptureManager import CaptureManager
 from configparser import ConfigParser
 
 
+programNumber=[2,3,4]
+trigger=14
+
 class VisionSensor:
 
     def __init__(self):
-
 
         # GPIO setup
         GPIO.setmode(GPIO.BCM)
 
         # Program Number
-        GPIO.setup(2, GPIO.IN)
-        GPIO.setup(3, GPIO.IN)
-        GPIO.setup(4, GPIO.IN)
-        GPIO.add_event_detect(2,GPIO.BOTH)
+        for i in programNumber:
+            GPIO.setup(i, GPIO.IN)
+            GPIO.add_event_detect(i,GPIO.BOTH)
+
+
+        # trigger
+        GPIO.setup(trigger, GPIO.IN)
+        GPIO.add_event_detect(trigger,GPIO.RISING)
 
         # VisionSensor setup
         self.frame = None
@@ -27,12 +36,43 @@ class VisionSensor:
         print(self.captureManager.programNumber)
 
 
-
     def getProgramNumber(self):
-        return int(str(int(GPIO.input(2)))+str(int(GPIO.input(3)))+str(int(GPIO.input(4))),2)
+        temp=str(int(GPIO.input(programNumber[0])))+str(int(GPIO.input(programNumber[1])))+\
+             str(int(GPIO.input(programNumber[2])))
+        return int(temp,2)
+
+    def resetEvent(self, pins,GPIOevent = GPIO.RISING):
+        if type(pins) is int:
+            GPIO.remove_event_detect(pins)
+            GPIO.add_event_detect(pins, GPIOevent)
+        else:
+            for i in pins:
+                GPIO.remove_event_detect(i)
+                GPIO.add_event_detect(i, GPIOevent)
 
     def loadConfig(self):
+        print('loadConfig ', self.captureManager.programNumber)
         pass
 
+    def run(self):
+        while True:
+            if GPIO.event_detected(programNumber[0]) or GPIO.event_detected(programNumber[1]) or \
+                    GPIO.event_detected(programNumber[2]):
+                visionSensor.resetEvent(programNumber, GPIO.BOTH)
+                visionSensor.captureManager.programNumber = visionSensor.getProgramNumber()
+                print(visionSensor.getProgramNumber())
+                visionSensor.loadConfig()
+            elif GPIO.event_detected(trigger):
+                visionSensor.resetEvent(trigger, GPIO.RISING)
+                print("execute tools")
 
-VisionSensor=VisionSensor()
+
+visionSensor = VisionSensor()
+visionSensor.run()
+
+
+        
+
+
+
+
